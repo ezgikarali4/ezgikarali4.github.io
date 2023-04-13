3b05783d3979a83260f92283be3761d96452920c
ezgikarali <ezgi.karali505@gmail.com>
2023-04-13 18:38:18 +0300

    bugfix

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const Container = styled.div`
   overflow-y: auto;
   scrollbar-width: none;
   color: white;
-  background: url("/public/img/bg.jpg");
+  background: url("./img/bg.jpg");
   &::-webkit-scrollbar {
     display: none;
   }
